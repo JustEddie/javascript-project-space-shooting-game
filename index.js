dbb3c56348ae81eeb46d4480db0b7ad335e07616
Eddie <skdpwls830@gmail.com>
2022-07-30 03:06:46 +1000

    need to plan before coding the rest

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 //canvas setting
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 let canvas;
 let ctx;
 
@@ -15,6 +16,11 @@ document.body.appendChild(canvas);
 
 let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 
+//spaceship (x,y) starting point : middle down on canvas
+let spaceshipX = canvas.width/2-32;
+let spaceshipY = canvas.height - 64;
+
+
 function loadImage(){
     backgroundImage = new Image();
     backgroundImage.src = "images/background.jpeg"
@@ -29,5 +35,33 @@ function loadImage(){
     enemyImage.src = 'images/enemyImage1.png'
 
     gameOverImage = new Image();
-    gameOverImage.src = 'images/gameOver.png'
+    gameOverImage.src = 'images/gameOver.jpeg'
 }
+
+//rendering image
+
+function render(){
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY)
+}
+
+function main(){
+    render()
+    requestAnimationFrame(main);
+}
+
+loadImage();
+main();
+
+
+//keypress spaceship move.. eventlistner...
+
+//more enemy...
+
+
+//score board
+//high score board .. every enemy dead add score
+//how can I store highest score ever with nick name input??
+
+
+//idea - credit : like : so I can store likes??
